92879d646ac44273499a0885f97c9f13b9a74270
cristiangpr <cristiangp777@gmail.com>
2020-05-28 10:06:46 -0500

    cors

--- pages/api/updates.js
+++ pages/api/updates.js
@@ -1,5 +1,19 @@
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+}
 module.exports = function (req, res) {
     console.log('JavaScript HTTP trigger function begun');
     var validationEventType = "Microsoft.EventGrid.SubscriptionValidationEvent";
